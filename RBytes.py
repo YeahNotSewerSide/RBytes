79cd65260ce9567d5e6f7631c7089824370a2f60
DoctorEenot <47860375+DoctorEenot@users.noreply.github.com>
2020-10-31 16:25:55 +0200

    RBytes

---
+++ RBytes.py
@@ -0,0 +1,176 @@
+def int_byte(number:int):   
+    return bytes((number,))
+
+
+class RBytes:
+    '''Fixed length of bytes'''
+    def __init__(self,array:bytes):
+        self._bytes = array
+
+    def __getitem__(self,item):
+        return self._bytes[item]
+
+    def __invert__(self):
+        to_return = b''
+        for i in range(len(self._bytes)):
+            to_return += int_byte(self._bytes[i] ^ 0b11111111)
+        return RBytes(to_return)
+
+    def __bool__(self):
+        to_return = False
+        for i in range(len(self._bytes)):
+            if self._bytes[i] != 0:
+                to_return = True
+                break
+        return to_return
+
+    def __len__(self):
+        return len(self._bytes)
+
+    def __rshift__(self,shift):
+        if shift<0:
+            raise ValueError('negative shift count')
+        if shift == 0:
+            return self
+
+        to_return = b''
+        buf = 0
+        if shift>8:
+            orig_shift = shift
+            shift = 8
+            to_return = RBytes(self._bytes)
+            for n in range(orig_shift//shift):
+                to_return = to_return>>8
+            
+            if orig_shift > (orig_shift//shift)*8:
+                shift = orig_shif - (orig_shift//shift)*8
+                to_return = to_return>>shift
+            return to_return
+        else:
+            for i in range(len(self._bytes)):
+                to_return += int_byte((self._bytes[i]>>shift)+buf)
+                buf = self._bytes[i]&((0b11111111>>(8-shift))<<(8-shift))
+        
+        return RBytes(to_return)
+
+    def __lshift__(self,shift):
+        if shift<0:
+            raise ValueError('negative shift count')
+        if shift == 0:
+            return self
+
+        to_return = b''
+        buf = 0
+
+        if shift>8:
+            orig_shift = shift
+            shift = 8
+            to_return = RBytes(self._bytes)
+            for n in range(orig_shift//shift):
+                to_return = to_return<<8
+            
+            if orig_shift > (orig_shift//shift)*8:
+                shift = orig_shif - (orig_shift//shift)*8
+                to_return = to_return<<shift
+            return to_return
+        else:
+            buf = 0
+            for i in range(len(self._bytes)-1,-1,-1):
+                to_return = int_byte(((self._bytes[i]&(0b11111111>>shift))<<shift)+buf) + to_return            
+                buf = (self._bytes[i]&((0b11111111>>(8-shift))<<(8-shift)))>>(8-shift)               
+
+        return RBytes(to_return)
+
+    def unsized_lshift(self,shift):
+        if shift<0:
+            raise ValueError('negative shift count')
+        if shift == 0:
+            return self
+        to_return = b''
+        if shift>8:
+            orig_shift = shift
+            shift = 8
+            to_return = RBytes(self._bytes)
+            for n in range(orig_shift//shift):
+                to_return = to_return.unsized_lshift(8)               
+            
+            if orig_shift > (orig_shift//shift)*8:
+                shift = orig_shif - (orig_shift//shift)*8
+                to_return = to_return.unsized_lshift(shift)
+            return to_return
+        else:
+            buf = 0
+            for i in range(len(self._bytes)-1,-1,-1):
+                to_return = int_byte(((self._bytes[i]&(0b11111111>>shift))<<shift)+buf) + to_return            
+                buf = (self._bytes[i]&((0b11111111>>(8-shift))<<(8-shift)))>>(8-shift) 
+               
+            if buf != 0:
+                to_return = int_byte(buf) + to_return
+
+        return RBytes(to_return)
+
+
+    def __add__(a,b):
+        if type(a) != RBytes or type(b)!= RBytes:
+            raise TypeError('Unsupported types of operands')
+        to_return = b''
+        
+        if len(b)>len(a):
+            buf = a
+            a = b
+            b = buf
+        
+        buf = b'\x00'*(len(a)-len(b)) + b._bytes
+        
+        b = RBytes(buf)
+
+        buf = 0
+
+        for i in range(len(a)-1,-1,-1):
+            to_return = int_byte((a[i]+b[i]+buf)&0b11111111) + to_return
+            buf = (a[i]+b[i]+buf)>>8
+
+        return RBytes(to_return)
+
+    def __and__(a,b):
+        if type(a) != RBytes or type(b)!= RBytes:
+            raise TypeError('Unsupported types of operands')
+        to_return = b''
+        
+        if len(b)>len(a):
+            buf = a
+            a = b
+            b = buf
+
+        buf = b'\x00'*(len(a)-len(b)) + b._bytes
+        b = RBytes(buf)
+
+        for i in range(len(a)-1,-1,-1):
+            to_return = int_byte(a[i] & b[i]) + to_return
+
+
+
+    
+#import time
+
+if __name__ == '__main__':
+    
+
+
+    bts = RBytes(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01')
+    #beg = time.time()
+    check = bts<<40
+    #delta = time.time()-beg
+    bts1 = RBytes(b'\x01')
+    new = bts + bts1
+    
+    print(bool(bts))
+    data = ~bts
+    print(data)
+    data = data >> 1
+    data = data.unsized_lshift(2)
+    #data = data << 2
+
+    if data:
+        print(bool(data))
+    
