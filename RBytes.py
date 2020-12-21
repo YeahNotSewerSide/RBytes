e5ca512210cb70d9c8bcfe9b73a6560fed878ef8
EeNot <47860375+DoctorEenot@users.noreply.github.com>
2020-12-21 04:41:14 +0200

    Added hexlifying

--- RBytes.py
+++ RBytes.py
@@ -2,9 +2,46 @@ def int_byte(number:int):
     return bytes((number,))
 
 
+hex_numbers={10:'a',
+             11:'b',
+             12:'c',
+             13:'d',
+             14:'e',
+             15:'f'}
+
+
+def dec_to_hex(num:int):
+    if num == 0:
+        return '00'
+    to_return = ''
+    while num > 0:
+        remainder = num%16
+        if remainder>9:
+            to_return = hex_numbers[remainder] + to_return
+        else:
+            to_return = str(remainder) + to_return 
+        num //= 16
+    if len(to_return) == 1:
+        to_return = '0'+to_return
+    return to_return
+
+
+
+def hexlify(b:bytes):
+    to_return=''
+    for byte in b:
+        to_return += dec_to_hex(byte)
+    return to_return
+
+
 class RBytes:
     '''Fixed length of bytes'''
     def __init__(self,array:bytes):
+        if type(array) != bytes:
+            if type(array) == RBytes:
+                self._bytes = array._bytes
+            else:
+                raise TypeError(f'Unsupported type {type(array)}')
         self._bytes = array
 
     def __getitem__(self,item):
@@ -53,6 +90,34 @@ class RBytes:
                 buf = (self._bytes[i]&((0b11111111>>(8-shift))))<<(8-shift)
         
         return RBytes(to_return)
+
+    def signed_rshift(self,shift):
+        if shift<0:
+            raise ValueError('negative shift count')
+        if shift == 0:
+            return self
+
+        to_return = b''
+        buf = 0
+        if shift>8:
+            orig_shift = shift
+            shift = 8
+            to_return = RBytes(self._bytes)
+            for n in range(orig_shift//shift):
+                to_return = to_return>>8
+            
+            if orig_shift > (orig_shift//shift)*8:
+                shift = orig_shift - (orig_shift//shift)*8
+                to_return = to_return>>shift
+            return to_return
+        else:
+            buf = 0
+            for i in range(len(self._bytes)):
+                to_return += int_byte((self._bytes[i]>>shift)+buf)
+                buf = (self._bytes[i]&((0b11111111>>(8-shift))))<<(8-shift)
+        
+        return RBytes(to_return)
+
 
     def __lshift__(self,shift):
         if shift<0:
@@ -214,6 +279,9 @@ class RBytes:
 
     def __bytes__(self):
         return self._bytes
+    
+    def __str__(self):
+        return hexlify(self._bytes)
 
 
 
@@ -224,24 +292,7 @@ if __name__ == '__main__':
     
     
 
-    bts = RBytes(b'\x40\x69\xe4\xf2\x01\x11\xff\x52')
-    data = bts >> 32
-    
-    print(bytes(bts))
-    #beg = time.time()
-    check = bts<<40
-    u = bts == bts1
-    #delta = time.time()-beg
-    bts1 = RBytes(b'\x01')
-    new = bts + bts1
-    
-    print(bool(bts))
-    data = ~bts
-    print(data)
-    data = data >> 1
-    data = data.unsized_lshift(2)
-    #data = data << 2
-
-    if data:
-        print(bool(data))
+    bts = RBytes(b'\xff\xff\xff\xe0\x00\x00\x0c\xff')
+    print(bts)
+    print(1)
     
